5df2d789c802de6b75ac257c0306babdf6e67fe7
Rémi Lavergne <rremilavergne@gmail.com>
2016-01-21 21:48:04 +0100

    update

--- rplugin/python/easy-cpp/__init__.py
+++ rplugin/python/easy-cpp/__init__.py
@@ -1,10 +1,14 @@
 import neovim
+import clang.cindex
 
 @neovim.plugin
 class Easy(object):
     def __init__(self, vim):
         self.vim = vim
+        self.index = clang.cindex.Index.create()
 
-    @neovim.command('Test')
-    def test(self):
-        self.vim.command('vsplit')
+    @neovim.command('EasyImpl')
+    def impl(self):
+        cfilename = self.vim.eval('expand("%:p")')
+        self.vim.current.line = cfilename
+
